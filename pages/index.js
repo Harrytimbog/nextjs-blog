33428f517fecccf8e56b4963cae474f1e88674e5
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-19 13:08:24 +0100

    Dynamic Routing

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,19 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -11,7 +23,8 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>
           Hello, I'm Timi. I am a software developer from Nigeria. You can view
-          my profile on <a href="https://github.com/harrytimbog">Github</a> and on{" "}
+          my profile on <a href="https://github.com/harrytimbog">Github</a> and
+          on{" "}
           <a href="https://www.linkedin.com/in/timilehin-arigbede-227449144/">
             LinkedIn
           </a>{" "}
@@ -21,6 +34,20 @@ export default function Home() {
             You can also contact me on Whatsapp
           </a>
         </div>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
