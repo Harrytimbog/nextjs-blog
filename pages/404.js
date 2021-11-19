6faa0cff3f76b8b85912b43247b1ecd64c1b000b
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-19 13:45:54 +0100

    Add a 404 page

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div style={{margin: 60}}>
+      <h1>404 - Page Not Found</h1>
+      <p>Get out immediately before I unleash the dogs</p>
+    </div>
+  );
+}
